1fc1c6b7fa6178ca2432aa0d695079824e1d88d9
samit1719 <samuelteshome1719@gmail.com>
2024-02-22 22:54:09 +0300

    added 0 file

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''2D matrix'''
+
+
+def rotate_2d_matrix(matrix):
+
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+
+            topLeft = matrix[top][left + i]
+
+            matrix[top][left + i] = matrix[bottom - i][left]
+
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+
+            matrix[bottom][right - i] = matrix[top + i][right]
+
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
