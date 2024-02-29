594828a18fee1a9bc7d444b4de67fe1a2c2c188d
samit1719 <samuelteshome1719@gmail.com>
2024-02-29 14:26:49 +0300

    added making changes file

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""determine the fewest number of coins needed
+   to meet a given amount total"""
+
+
+def makeChange(coins, total):
+    """Return: fewest number of coins needed to meet total
+    """
+    if total <= 0:
+        return 0
+
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
